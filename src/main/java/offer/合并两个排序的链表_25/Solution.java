60a23ba13d353d9fe9e1429f6bce8b061a0d3ccb
winwto <liuxuenbrw@gmail.com>
2021-04-23 16:47:51 +0800

    剑指offer_合并两个排序的链表_25

---
+++ src/main/java/offer/合并两个排序的链表_25/Solution.java
@@ -0,0 +1,33 @@
+package offer.合并两个排序的链表_25;
+
+import com.sun.corba.se.spi.ior.IORFactories;
+import leetcode.linkedList.ListNode;
+
+public class Solution {
+    // 1->2->4
+    // 1->3->4
+    // 1->1->2->3->4->4
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return res.next;
+    }
+}
