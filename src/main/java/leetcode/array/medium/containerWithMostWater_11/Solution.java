243634edf96e1ccb7ab5b0d6f6aa97dd44194431
CSGOINGTO <liuxuenbrw@163.com>
2021-05-04 14:32:12 +0800

    盛最多水的容器_11

--- src/main/java/leetcode/array/medium/containerWithMostWater_11/Solution.java
+++ src/main/java/leetcode/array/medium/containerWithMostWater_11/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
 
     /**
      * 暴力解法
+     *
      * @param height
      * @return
      */
@@ -23,20 +24,41 @@ public class Solution {
 
     /**
      * 双指针法
+     *
      * @param height
      * @return
      */
     public int maxArea1(int[] height) {
-        if(height.length <= 1) {
+        if (height.length <= 1) {
             return 0;
         }
         int max = Integer.MIN_VALUE;
         int left = 0;
         int right = height.length - 1;
-        while(left < right) {
-            int minHeight = height[left] < height[right] ? height[left ++] : height[right --];
+        while (left < right) {
+            int minHeight = height[left] < height[right] ? height[left++] : height[right--];
             max = Math.max(max, minHeight * (right - left + 1));
         }
         return max;
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(1)
+     */
+    public int maxArea2(int[] height) {
+        if (height == null || height.length == 0) return 0;
+        int left = 0;
+        int right = height.length - 1;
+        int res = 0;
+        while (left != right) {
+            res = Math.max(res, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
     }
 }
