06887c88fd62461bf0bf24ea7715a1cd3b11d006
winwto <liuxuenbrw@gmail.com>
2021-05-12 15:27:07 +0800

    环形链表_141

--- src/main/java/leetcode/linkedList/easy/linkedListCycle_141/Solution.java
+++ src/main/java/leetcode/linkedList/easy/linkedListCycle_141/Solution.java
@@ -2,6 +2,9 @@ package leetcode.linkedList.easy.linkedListCycle_141;
 
 import leetcode.linkedList.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -14,15 +17,35 @@ public class Solution {
         l3.next = l4;
         l4.next = l2;
         Solution solution = new Solution();
-        System.out.println(solution.hasCycle(l1));
+        System.out.println(solution.hasCycle1(l1));
     }
 
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(1)
+     */
     public boolean hasCycle(ListNode head) {
         if (head == null || head.next == null) return false;
         ListNode listNode = head.next.next;
         while (listNode != null && listNode.next != null) {
             if (listNode == head) return true;
             listNode = listNode.next.next;
+            head = head.next;
+        }
+        return false;
+    }
+
+    /**
+     * 时间复杂度O(n)
+     * 空间复杂度O(n)
+     */
+    public boolean hasCycle1(ListNode head) {
+        if (head == null || head.next == null) return false;
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) {
+                return true;
+            }
             head = head.next;
         }
         return false;
