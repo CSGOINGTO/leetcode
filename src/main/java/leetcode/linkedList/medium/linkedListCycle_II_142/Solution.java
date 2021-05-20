1276548b830fb9d746a4700946a3979f19795abf
winwto <liuxuenbrw@gmail.com>
2021-05-20 16:21:35 +0800

    环形链表 II_142

--- src/main/java/leetcode/linkedList/medium/linkedListCycle_II_142/Solution.java
+++ src/main/java/leetcode/linkedList/medium/linkedListCycle_II_142/Solution.java
@@ -6,6 +6,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(3);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(0);
+        ListNode l4 = new ListNode(-4);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l2;
+        solution.detectCycle1(l1);
+    }
+
     public ListNode detectCycle(ListNode head) {
         if (head == null) return null;
         Set<ListNode> nodeSet = new HashSet<>();
@@ -17,5 +31,23 @@ public class Solution {
             head = head.next;
         }
         return null;
+    }
+
+    public ListNode detectCycle1(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode fastNode = head;
+        ListNode slowNode = head;
+        while (true) {
+            if (fastNode == null || fastNode.next == null) return null;
+            fastNode = fastNode.next.next;
+            slowNode = slowNode.next;
+            if (fastNode == slowNode) break;
+        }
+        fastNode = head;
+        while (fastNode != slowNode) {
+            slowNode = slowNode.next;
+            fastNode = fastNode.next;
+        }
+        return fastNode;
     }
 }
