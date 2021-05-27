a34e106b7a9e3e174aa5fe6c14e83701bfc27143
winwto <liuxuenbrw@gmail.com>
2021-05-27 18:32:05 +0800

    合并区间_56

--- src/main/java/leetcode/array/medium/mergeIntervals_56/Solution.java
+++ src/main/java/leetcode/array/medium/mergeIntervals_56/Solution.java
@@ -7,10 +7,10 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[][] nums = new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-        System.out.println(Arrays.deepToString(solution.merge1(nums)));
+        System.out.println(Arrays.deepToString(solution.merge(nums)));
     }
 
-    public int[][] merge1(int[][] intervals) {
+    public int[][] merge(int[][] intervals) {
         List<int[]> res = new ArrayList<>();
         if (intervals == null || intervals.length == 0) return res.toArray(new int[0][]);
         // 对起点终点进行排序
@@ -30,40 +30,5 @@ public class Solution {
             i++;
         }
         return res.toArray(new int[0][]);
-    }
-
-    public int[][] merge(int[][] intervals) {
-        Set<int[]> res = new HashSet<>();
-        Map<Integer, int[]> startMap = new HashMap<>();
-        Map<Integer, int[]> endMap = new HashMap<>();
-        for (int i = 0; i < intervals.length; i++) {
-            startMap.put(intervals[i][0], intervals[i]);
-            endMap.put(intervals[i][1], intervals[i]);
-        }
-        for (Map.Entry<Integer, int[]> startEntry : startMap.entrySet()) {
-            int[] tmp = null;
-            for (Map.Entry<Integer, int[]> endEntry : endMap.entrySet()) {
-                int[] startArray = startEntry.getValue();
-                int[] endArray = endEntry.getValue();
-                if (startArray[1] >= endArray[0]) {
-                    if (tmp == null) {
-                        tmp = new int[2];
-                        tmp[0] = startArray[0];
-                        tmp[1] = endArray[1];
-                    } else {
-                        if (tmp[1] < endArray[1]) {
-                            tmp[1] = endArray[1];
-                        }
-                    }
-                }
-                if (tmp != null) res.add(tmp);
-            }
-        }
-        int[][] resArray = new int[res.size()][2];
-        int index = 0;
-        for (int[] a : res) {
-            resArray[index++] = a;
-        }
-        return resArray;
     }
 }
