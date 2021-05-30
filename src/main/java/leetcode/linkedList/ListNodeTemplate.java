e85ecf6811382cd751152f09a99fc3cfea8d774a
CSGOINGTO <liuxuenbrw@163.com>
2021-05-30 22:42:54 +0800

    获取链表结构工具类

---
+++ src/main/java/leetcode/linkedList/ListNodeTemplate.java
@@ -0,0 +1,29 @@
+package leetcode.linkedList;
+
+public class ListNodeTemplate {
+
+    public static ListNode getList() {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        return l1;
+    }
+
+    public static ListNode getList1() {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        return l1;
+    }
+
+}
