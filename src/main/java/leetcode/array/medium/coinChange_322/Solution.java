f2730047b01dc8619ce6d05cc31d07a2a6bbbc9f
CSGOINGTO <liuxuenbrw@163.com>
2021-07-04 23:28:31 +0800

    零钱兑换_322

--- src/main/java/leetcode/array/medium/coinChange_322/Solution.java
+++ src/main/java/leetcode/array/medium/coinChange_322/Solution.java
@@ -13,6 +13,45 @@ public class Solution {
     int[] memo;
 
     public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0) return -1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+        return dp[amount] == (amount + 1) ? -1 : dp[amount];
+    }
+
+    public int coinChange2(int[] coins, int amount) {
+        // 自底向上的动态规划
+        if (coins.length == 0) {
+            return -1;
+        }
+
+        // memo[n]的值： 表示的凑成总金额为n所需的最少的硬币个数
+        int[] memo = new int[amount + 1];
+        memo[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0 && memo[i - coins[j]] < min) {
+                    min = memo[i - coins[j]] + 1;
+                }
+            }
+            // memo[i] = (min == Integer.MAX_VALUE ? Integer.MAX_VALUE : min);
+            memo[i] = min;
+        }
+
+        return memo[amount] == Integer.MAX_VALUE ? -1 : memo[amount];
+    }
+
+    public int coinChange1(int[] coins, int amount) {
         if (coins.length == 0) {
             return -1;
         }
